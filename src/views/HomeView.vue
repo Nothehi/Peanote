64cb6943149da0182c5fb553b5402c2061a2eeb9
nothehi <nodehi7813@gmail.com>
2023-12-26 10:24:03 +0330

    fear: add home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
+import { RouterLink } from 'vue-router'
 </script>
 
 <template>
-  <main>
+  <main class="flex flex-col items-center justify-center">
+    <h1 class="p-20 text-4xl font-bold text-primary">
+      Peanote
+    </h1>
+
+    <router-link to="/game" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface">Play</router-link>
+    <router-link to="#" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface">Records</router-link>
   </main>
 </template>
