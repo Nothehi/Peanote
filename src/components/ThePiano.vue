4b67f68b36fb0d0df382dcba2262f64a07efd573
nothehi <nodehi7813@gmail.com>
2023-12-26 10:24:20 +0330

    feat: add piano and staff components

---
+++ src/components/ThePiano.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box-border relative flex flex-row items-start justify-start w-full h-full">
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+    <button
+      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[10%] border-surface border-2 focus:bg-stone-900"
+    ></button>
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+    <button
+      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[24%] border-surface border-2 focus:bg-stone-900"
+    ></button>
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+    <button
+      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[53%] border-surface border-2 focus:bg-stone-900"
+    ></button>
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+    <button
+      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[67%] border-surface border-2 focus:bg-stone-900"
+    ></button>
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+    <button
+      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[82%] border-surface border-2 focus:bg-stone-900"
+    ></button>
+    <button
+      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
+    ></button>
+  </div>
+</template>
---
+++ src/components/TheStaff.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full space-y-5">
+    <div class="w-full border-t border-primary"></div>
+    <div class="w-full border-t border-primary"></div>
+    <div class="w-full border-t border-primary"></div>
+    <div class="w-full border-t border-primary"></div>
+    <div class="w-full border-t border-primary"></div>
+  </div>
+</template>
