0dcf8f6a9c3aacf8e21f85691f879ffdc4398448
nothehi <nodehi7813@gmail.com>
2023-12-26 13:47:53 +0330

    style: format the home page style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,11 +4,13 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <main class="flex flex-col items-center justify-center">
-    <h1 class="p-20 text-4xl font-bold text-primary">
-      Peanote
-    </h1>
+    <h1 class="p-20 text-4xl font-bold text-primary">Peanote</h1>
 
-    <router-link to="/game" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface">Play</router-link>
-    <router-link to="#" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface">Records</router-link>
+    <router-link to="/game" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface"
+      >Play</router-link
+    >
+    <router-link to="#" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface"
+      >Records</router-link
+    >
   </main>
 </template>
