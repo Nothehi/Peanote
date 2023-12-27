f90bba0587ab01c5167313e118874bbbf307f8f6
nothehi <nodehi7813@gmail.com>
2023-12-27 14:50:39 +0330

    style: fix style and size in home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,12 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <main class="flex flex-col items-center justify-center">
-    <h1 class="p-20 text-4xl font-bold text-primary">Peanote</h1>
+    <h1 class="my-24 text-6xl font-bold text-center text-primary">Peanote</h1>
 
-    <router-link to="/game" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface"
+    <router-link to="/game" class="px-4 py-2 mt-4 text-2xl rounded-xl bg-primary text-surface"
       >Play</router-link
     >
-    <router-link to="#" class="px-4 py-2 mt-4 rounded-xl bg-primary text-surface"
+    <router-link to="/" class="px-4 py-2 mt-4 text-2xl rounded-xl bg-primary text-surface"
       >Records</router-link
     >
   </main>
