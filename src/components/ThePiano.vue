60ff9c7d4eb1fe030a3a341a324d3e6d3f2df2c9
nothehi <nodehi7813@gmail.com>
2024-03-29 12:08:04 +0330

    style: change piano css style

--- src/components/ThePiano.vue
+++ src/components/ThePiano.vue
@@ -5,12 +5,12 @@ const emit = defineEmits<{
   (e: 'keyPushed', keyName: string): void
 }>()
 
-const synth = new Tone.Synth().toDestination();
+const synth = new Tone.Synth().toDestination()
 
 function keyPushed(keyName: string) {
   emit('keyPushed', keyName)
-  
-  synth.triggerAttackRelease(keyName+'4', '8n')
+
+  synth.triggerAttackRelease(keyName + '4', '8n')
 }
 </script>
 
@@ -18,51 +18,51 @@ function keyPushed(keyName: string) {
   <div class="box-border relative flex flex-row items-start justify-start w-full h-full">
     <button
       @click="() => keyPushed('c')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('c#')"
-      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[10%] border-surface border-2 focus:bg-stone-900"
-    ></button>
+      class="rounded-lg bg-stone-700 z-10 shadow-neuburtal-lg shadow-stone-800 border-2 border-stone-800 h-1/2 absolute w-1/12 left-[10%] -top-1 active:shadow-none active:border-gray-800 active:bg-gray-950 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('d')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('d#')"
-      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[24%] border-surface border-2 focus:bg-stone-900"
-    ></button>
+      class="rounded-lg bg-stone-700 z-10 shadow-neuburtal-lg shadow-stone-800 border-2 border-stone-800 h-1/2 absolute w-1/12 left-[24%] -top-1 active:shadow-none active:border-gray-800 active:bg-gray-950 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('e')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('f')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('f#')"
-      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[53%] border-surface border-2 focus:bg-stone-900"
-    ></button>
+      class="rounded-lg bg-stone-700 z-10 shadow-neuburtal-lg shadow-stone-800 border-2 border-stone-800 h-1/2 absolute w-1/12 left-[53%] -top-1 active:shadow-none active:border-gray-800 active:bg-gray-950 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('g')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('g#')"
-      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[67%] border-surface border-2 focus:bg-stone-900"
-    ></button>
+      class="rounded-lg bg-stone-700 z-10 shadow-neuburtal-lg shadow-stone-800 border-2 border-stone-800 h-1/2 absolute w-1/12 left-[67%] -top-1 active:shadow-none active:border-gray-800 active:bg-gray-950 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('a')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('a#')"
-      class="rounded-lg bg-black h-1/2 absolute w-1/12 left-[82%] border-surface border-2 focus:bg-stone-900"
-    ></button>
+      class="rounded-lg bg-stone-700 z-10 shadow-neuburtal-lg shadow-stone-800 border-2 border-stone-800 h-1/2 absolute w-1/12 left-[82%] -top-1 active:shadow-none active:border-gray-800 active:bg-gray-950 active:translate-y-2">
+    </button>
     <button
       @click="() => keyPushed('b')"
-      class="w-full h-full bg-white border-2 rounded-lg border-surface focus:bg-stone-300"
-    ></button>
+      class="w-full h-full bg-white border-2 border-gray-200 rounded-lg shadow-neuburtal-lg shadow-gray-200 active:shadow-none active:border-sky-200 active:bg-sky-50 active:translate-y-2">
+    </button>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,13 @@ export default {
         primary: '#FEECBC',
         secondary: '#999079',
         tertiary: '#A5A09C',
-      }
+      },
+      boxShadow: {
+				'neuburtal-sm': '0 .125rem 0rem rgba(0, 0, 0, 1)',
+				'neuburtal': '0 .25rem 0rem rgba(0, 0, 0, 1)',
+				'neuburtal-md': '0 .375rem 0rem rgba(0, 0, 0, 1)',
+				'neuburtal-lg': '0 .5rem 0rem rgba(0, 0, 0, 1)',
+			},
     },
   },
   plugins: [],
