1e1356a92dd31bec4c593d732347eec911aa0009
nothehi <nodehi7813@gmail.com>
2025-03-10 20:17:57 +0330

    feat: add app header

---
+++ app/components/header.tsx
@@ -0,0 +1,34 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import Icon from "@/components/ui/icon"
+
+type Props = {
+	name: string
+	level: number
+  avatar: string
+}
+
+export default function Header({name, level, avatar}: Props) {
+	return (
+		<header className="flex flex-row items-center justify-start w-full p-4 bg-cherry-800 rounded-b-4xl">
+			<Avatar className="max-w-12 size-full ring-4 ring-cherry-900">
+				<AvatarImage src={avatar} alt={name} />
+				<AvatarFallback>{name.charAt(0)}</AvatarFallback>
+			</Avatar>
+
+			<div className="flex flex-col items-start justify-center mr-2">
+				<h5 className="text-lg font-bold text-white font-borna">
+					{name}
+					<span className="text-sm font-light text-cherry-100"> [صبحت بخیر👋]</span>
+				</h5>
+
+				<h6 className="text-xs text-cherry-100 font-borna">سطح {level}</h6>
+			</div>
+
+			<Icon className="p-2 mr-auto rounded-full bg-cherry-900" name="notification" color="fill-cherry-200" />
+		</header>
+	)
+}
